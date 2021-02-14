f77dd8b519d22e1c56a22458699c9529c07cca44
bartoszmajczak <bartosz.szymon.majczak@gmail.com>
2021-02-14 18:55:37 +0100

    *Update* url decode response from route parameters

--- source/Router.php
+++ source/Router.php
@@ -19,21 +19,21 @@ class Router {
     $this -> response = new Response;
   }
 
-  public function route($method, $arguments) {
+  public function route($method, $arguments) : void {
     $route = array_shift($arguments);
     $callback = array_pop($arguments);
 
     if ($method == $this -> request -> method) {
       preg_match($route, $this -> request -> route, $matches);
 
-      if (!empty($matches)) {
+      if ($matches) {
         $parameters = [];
 
         foreach ($matches as $key => $value) {
-          is_string($key) ? $parameters[$key] = $value : null;
+          is_string($key) ? $parameters[$key] = urldecode($value) : null;
         }
 
-        !empty($parameters) ? $this -> request -> parameters = $parameters : null;
+        $parameters ? $this -> request -> parameters = $parameters : null;
 
         foreach ($arguments as $key => $value) {
           $return = $value($this -> request, $this -> response, $this -> data);
